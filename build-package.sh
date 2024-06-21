f1386725162b58039e6c82e15740fdd249931a3c
沐川 <43491106+mucute@users.noreply.github.com>
2024-06-21 20:12:24 +0800

    Update build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -477,7 +477,7 @@ while (($# >= 1)); do
 		-i)
 			if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
 				termux_error_exit "./build-package.sh: option '-i' is not available for on-device builds"
-			elif [ "$TERMUX_PREFIX" != "/data/data/com.termux/files/usr" ]; then
+			elif [ "$TERMUX_PREFIX" != "/data/data/cn.mucute.simple/linux" ]; then
 				termux_error_exit "./build-package.sh: option '-i' is available only when TERMUX_APP_PACKAGE is 'com.termux'"
 			else
 				export TERMUX_INSTALL_DEPS=true
